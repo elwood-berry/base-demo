094a40e422cfa5ad27ef33fed390069b4c91bb90
elwood-berry <eberry@anterasoftware.com>
2021-06-25 16:05:47 -0500

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,40 +18,32 @@ import { MatBadgeModule } from '@angular/material/badge'; // BADGE
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet'; // BOTTOM SHEET
 import { MatButtonModule } from '@angular/material/button'; // BUTTON
 import { MatButtonToggleModule } from '@angular/material/button-toggle'; // BUTTON SYLE
-
 import { MatCardModule } from '@angular/material/card'; // CARD
 import { MatCheckboxModule } from '@angular/material/checkbox'; // CHECKBOX
 import { MatChipsModule } from '@angular/material/chips'; // CHIPS
 import { MatDatepickerModule } from '@angular/material/datepicker'; // DATE PICKER
 import { MatDialogModule } from '@angular/material/dialog'; // DIALOG
-
 import { MatDividerModule } from '@angular/material/divider'; // DIVIDER
-
 import { DragDropModule } from '@angular/cdk/drag-drop'; // DRAG & DROP
-
 import { MatExpansionModule } from '@angular/material/expansion'; // EXPANSION
 import { MatFormFieldModule } from '@angular/material/form-field'; // FORM FIELD
 import { MatGridListModule } from '@angular/material/grid-list'; // GRID LIST
 import { MatIconModule } from '@angular/material/icon'; // ICON
-
 import { MatInputModule } from '@angular/material/input'; // INPUT
 import { MatListModule } from '@angular/material/list'; // LIST
 import { MatMenuModule } from '@angular/material/menu'; // MENU
 import { MatPaginatorModule } from '@angular/material/paginator'; // PAGINATOR
 import { MatProgressBarModule } from '@angular/material/progress-bar'; // PROGRESS BAR
-
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; // PROGRESS SPINNER
 import { MatRadioModule } from '@angular/material/radio'; // RADIO BUTTONS
 import { MatRippleModule } from '@angular/material/core'; // RIPPLE
 import { MatSelectModule } from '@angular/material/select'; // SELECT
 import { MatSidenavModule } from '@angular/material/sidenav'; // SIDENAV
-
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'; // SLIDE TOGGLE
 import { MatSliderModule } from '@angular/material/slider'; // SLIDER
 import { MatSnackBarModule } from '@angular/material/snack-bar'; // SNACKBAR
 import { MatSortModule } from '@angular/material/sort'; // SORT
 import { MatStepperModule } from '@angular/material/stepper'; // STEPPER
-
 import { MatTableModule } from '@angular/material/table'; // TABLE
 import { MatTabsModule } from '@angular/material/tabs'; // TABS
 import { MatToolbarModule } from '@angular/material/toolbar'; // TOOLBAR
@@ -60,6 +52,9 @@ import { MatTreeModule } from '@angular/material/tree';
 
 // FXLAYOUT
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+// COMPONENTS
+import { AdvanceDialogComponent } from './components/advance-dialog/advance-dialog.component';
 
 @NgModule({
   imports: [
@@ -101,7 +96,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     DragDropModule,
     FlexLayoutModule
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, AdvanceDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/components/advance-dialog/advance-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advance-dialog',
+  templateUrl: './advance-dialog.component.html',
+  styleUrls: ['./advance-dialog.component.css']
+})
+export class AdvanceDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
